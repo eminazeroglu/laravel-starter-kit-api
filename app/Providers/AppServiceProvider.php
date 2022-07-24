0de1a79fff62c5606e28602a2bdd24c7a3de1998
Emin Azeroglu <azeroglu.developer@gmail.com>
2022-07-24 23:38:40 +0400

    fix: Deploy bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 use App\Http\Routing\ApiResourceRegister;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Database\Events\MigrationsStarted;
 use Illuminate\Routing\ResourceRegistrar;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,7 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Event::listen(MigrationsStarted::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        });
+
+        Event::listen(MigrationsEnded::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=1');
+        });
     }
 
     /**
@@ -23,11 +34,15 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         $register = new ApiResourceRegister($this->app['router']);
         $this->app->bind(ResourceRegistrar::class, static function () use ($register) {
             return $register;
         });
+
+        if (app()->environment() !== 'local'):
+            $url->forceScheme('https');
+        endif;
     }
 }
