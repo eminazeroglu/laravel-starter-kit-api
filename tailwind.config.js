fbc5b0ff2f1c8257d5466885bcf9e5d48cae0185
Emin Azeroglu <azeroglu.developer@gmail.com>
2022-10-01 14:34:36 +0400

    fix: refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    darkMode: "class",
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
@@ -16,34 +17,7 @@ module.exports = {
         },
         extend: {
             colors: {
-                brand: {
-                    DEFAULT: '#01847F',
-                    dark: '#0F5C5A',
-                    900: '#1A908C',
-                    800: '#349D99',
-                    700: '#4DA9A5',
-                    600: '#67B5B2',
-                    500: '#80C2BF',
-                    400: '#99CECC',
-                    300: '#B3DAD9',
-                    200: '#CCE6E5',
-                    100: '#E6F3F2',
-                    50: '#F2F9F9',
-                },
-                brandSecondary: {
-                    DEFAULT: '#700E19',
-                    dark: '#500a11',
-                    900: '#7E2630',
-                    800: '#8D3E47',
-                    700: '#9B565E',
-                    600: '#A96E75',
-                    500: '#B8878C',
-                    400: '#C69FA3',
-                    300: '#D4B7BA',
-                    200: '#E2CFD1',
-                    100: '#F1E7E8',
-                    50: '#F8F3F4',
-                }
+
             },
         },
     },
