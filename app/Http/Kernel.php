97503e1c3668691443d1187665f0dbcd56666068
Emin Azeroglu <azeroglu.developer@gmail.com>
2022-11-19 17:04:14 +0400

    fix: refactoring

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,14 @@ class Kernel extends HttpKernel
     ];
 
     protected $middlewareGroups = [
-        'web' => [],
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
