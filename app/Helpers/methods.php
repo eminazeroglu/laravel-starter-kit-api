bf4f132a9b6212f78947333abe45718febaab5a6
Emin Azeroglu <azeroglu.developer@gmail.com>
2022-12-25 15:50:10 +0400

    fix: refactoring

--- app/Helpers/methods.php
+++ app/Helpers/methods.php
@@ -4,3 +4,8 @@ function helper(): \App\Helpers\Helper
 {
     return (new \App\Helpers\Helper);
 }
+
+function translate($key = null, $search = null, $replace = null)
+{
+    return helper()->translate($key, $search, $replace);
+}
